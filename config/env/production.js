0e2c3e331e3520fca3a964594834baf94077283c
Janne Saarela <janne.saarela@kehittamo.fi>
2016-07-12 17:55:22 +0300

    Edit mongourl logic

--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,7 @@
+const mongoUrl = process.env.MONGOHQ_URL ? process.env.MONGOHQ_URL : "mongodb://localhost/express-mongoose-es6-rest-api-production";
+
 export default {
     env: "production",
-    db: process.env.MONGOHQ_URL ? process.env.MONGOHQ_URL : "mongodb://localhost/express-mongoose-es6-rest-api-production",
+    db: mongoUrl,
     port: 3000,
 };
--- dist/config/env/production.js
+++ dist/config/env/production.js
@@ -3,9 +3,11 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
+var mongoUrl = process.env.MONGOHQ_URL ? process.env.MONGOHQ_URL : "mongodb://localhost/express-mongoose-es6-rest-api-production";
+
 exports.default = {
     env: "production",
-    db: process.env.MONGOHQ_URL ? process.env.MONGOHQ_URL : "mongodb://localhost/express-mongoose-es6-rest-api-production",
+    db: mongoUrl,
     port: 3000
 };
 module.exports = exports["default"];
