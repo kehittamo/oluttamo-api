9c15bce6aef43ced3c1e2024fb899e519b12a10e
Janne Saarela <janne.saarela@kehittamo.fi>
2016-07-12 18:21:14 +0300

    Add port logic

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
-const mongoUrl = process.env.MONGOHQ_URL ? process.env.MONGOHQ_URL : "mongodb://localhost/express-mongoose-es6-rest-api-production";
-
+const db = process.env.MONGOHQ_URL ? process.env.MONGOHQ_URL : "mongodb://localhost/express-mongoose-es6-rest-api-production";
+const port = process.env.PORT ? process.env.PORT : 3000;
 export default {
     env: "production",
-    db: mongoUrl,
-    port: 3000,
+    db,
+    port,
 };
--- dist/config/env/production.js
+++ dist/config/env/production.js
@@ -3,12 +3,12 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-var mongoUrl = process.env.MONGOHQ_URL ? process.env.MONGOHQ_URL : "mongodb://localhost/express-mongoose-es6-rest-api-production";
-
+var db = process.env.MONGOHQ_URL ? process.env.MONGOHQ_URL : "mongodb://localhost/express-mongoose-es6-rest-api-production";
+var port = process.env.PORT ? process.env.PORT : 3000;
 exports.default = {
     env: "production",
-    db: mongoUrl,
-    port: 3000
+    db: db,
+    port: port
 };
 module.exports = exports["default"];
 //# sourceMappingURL=production.js.map
