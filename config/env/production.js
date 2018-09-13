d4370d7ea52f044aa7cbd73c735e75de66029158
Janne Saarela <janne.juhani@gmail.com>
2018-09-13 09:27:52 +0300

    Change prod mongo env

--- config/env/production.js
+++ config/env/production.js
@@ -1,4 +1,6 @@
-const db = process.env.MONGOHQ_URL ? process.env.MONGOHQ_URL : "mongodb://localhost/express-mongoose-es6-rest-api-production";
+const db = process.env.MONGODB_URI
+    ? process.env.MONGODB_URI
+    : "mongodb://localhost/express-mongoose-es6-rest-api-production";
 const port = process.env.PORT ? process.env.PORT : 3000;
 export default {
     env: "production",
